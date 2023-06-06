645dc7bc97980de6cc61a803a64f0503cee08fab
Jilo <116283418+jeanloick@users.noreply.github.com>
2023-06-06 16:39:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,3 +19,19 @@ class Guitar (BaseModel):
 @app.get("/message")
 async def root():
     return {"message":"with great power comes great responsibility"}
+
+guitarList = [{"guitarBrand":"Gibson", "guitarType":"electric", "guitarName": "flying V 1966", "guitarPrice":1350, "guitarColor": "black"},
+              {"guitarBrand":"Gibson","guitarType":"electric", "guitarName": "SG signature Angus YOUNG 1988", "guitarPrice":2988, "guitarColor": "red"}]
+
+@app.get("/guitars")
+async def getGuitars():
+    return { "Guitars": guitarList,
+              "limits": 10,
+              "total": 2,
+              "skip": 0
+        }
+@app.post("/guitars")
+async def create_post(payload: Guitar):
+    print(payload.guitarName)
+    guitarList.append(payload.dict())
+    return {"message": f"guitar sucessfully added to the flow of metal and rock and roll, his name is : {payload.guitarBrand +' '+ payload.guitarName}, such a great choice ;)"}
