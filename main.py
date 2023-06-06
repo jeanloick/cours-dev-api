735642d1366ea5eb649867148687ea72d7917168
Jilo <116283418+jeanloick@users.noreply.github.com>
2023-06-06 16:37:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+
+
 app = FastAPI() #variable names for the server
 
-@app.get("/")
+#Data models / Schema / DTO
+class Guitar (BaseModel):
+    guitarBrand: str
+    guitarType:str
+    guitarName: str
+    guitarColor: str
+    guitarPrice: float
+    #availability: bool = True #default / optionnel
+    #rating: Optional[int] # completement optionnel
+
+
+@app.get("/message")
 async def root():
     return {"message":"with great power comes great responsibility"}
