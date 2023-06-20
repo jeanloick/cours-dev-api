68a53751e271a0bcdf05ca358af8aab4b24872ba
Jilo <116283418+jeanloick@users.noreply.github.com>
2023-06-20 17:00:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ from typing import Optional
 from fastapi import FastAPI, Body, HTTPException, Response, status
 from pydantic import BaseModel
 
+import psycopg2
+from psycopg2.extras import  RealDictCursor
+
+# Connexion DB
+
+connexion = psycopg2.connect(
+    host="dpg-ci8rn3liuie0h35b9g30-a.frankfurt-postgres.render.com",
+    database="guitar_center_render",
+    user="geralt",
+    password="slSywIr6hJQXXk72eaC0bewLDpA9qvgk",
+    cursor_factory=RealDictCursor
+)
+cursor= connexion.cursor() # TODO  modifs à faire
+
 api_description = description = """
 Guitar API helps you to be a better guitarist
 
@@ -41,7 +55,7 @@ class Guitar (BaseModel):
     guitarName: str
     guitarColor: str
     guitarPrice: float
-    #availability: bool = True #default / optionnel
+    availability: bool = False #default / optionnel
     #rating: Optional[int] # completement optionnel
 
 
@@ -54,15 +68,21 @@ guitarList = [{"guitarBrand":"Gibson", "guitarType":"electric", "guitarName": "f
 
 @app.get("/guitars", tags=["Guitars"])
 async def getGuitars():
-    return { "Guitars": guitarList,
-              "limits": 10,
-              "total": 2,
-              "skip": 0
+
+    cursor.execute("SELECT * FROM guitar")
+
+    dbGuitars= cursor.fetchall()
+
+    return { "Guitars":  dbGuitars,
+              #"limits": 10,
+              #"total": 2,
+             #"skip": 0
         }
 @app.post("/guitars", tags=["Guitars"])
 async def create_guitar(payload: Guitar, response: Response, tags=["Guitars"]):
-    print(payload.guitarName)
-    guitarList.append(payload.dict())
+   # cursor.execute(f"INSERT INTO guitar (name,brand,type,color,price,availability) VALUES ('{payload.guitarName}','{payload.guitarBrand}','{payload.guitarType}','{payload.guitarColor}',{payload.guitarPrice},{payload.availability}) RETURNING*;")
+    cursor.execute ("INSERT INTO guitar (name,brand,type,color,price,availability) VALUES (%s,%s,%s,%s,%s,%s) RETURNING*;", (payload.guitarName, payload.guitarBrand, payload.guitarType, payload.guitarColor, payload.guitarPrice, payload.availability))
+    connexion.commit() # Sauvegarde dans la base de donnée
 
     response.status_code = status.HTTP_201_CREATED
     return {"message": f"Guitar sucessfully added to the flow of metal and rock and roll, his name is : {payload.guitarBrand +' '+ payload.guitarName}, such a great choice ;)"}
@@ -70,13 +90,21 @@ async def create_guitar(payload: Guitar, response: Response, tags=["Guitars"]):
 @app.get("/guitars/{guitarId}", tags=["Guitars"])
 async def getGuitar(guitarId: int, response: Response, tags=["Guitars"]):
     try: 
-        correspondingGuitar = guitarList[guitarId -1]# pck ID begin to 1 and index to 0 
-        return correspondingGuitar 
+        cursor.execute(f"SELECT * FROM guitar WHERE id={guitarId}")
+        correspondingGuitar = cursor.fetchone()
+        if(correspondingGuitar):
+            return correspondingGuitar
+        else:
+            raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail= "Guitar not found, guitar lost into the void T-T"
+        )
+            
     
     except:
         raise HTTPException(
             status.HTTP_404_NOT_FOUND,
-            detail= "GUitar not found, guitar lost into the void T-T"
+            detail= "Guitar not found, guitar lost into the void T-T"
         )
     
 
@@ -85,25 +113,29 @@ async def getGuitar(guitarId: int, response: Response, tags=["Guitars"]):
 @app.delete("/guitars/{guitarId}", tags=["Guitars"])
 async def deleteGuitar(guitarId: int, response: Response):
     try: 
-        guitarList.pop(guitarId -1)
-        response.status_code = status.HTTP_204_NO_CONTENT
-        return 
+         cursor.execute(
+        'DELETE FROM guitar WHERE id=%s RETURNING *', (guitarId,)
+        )
+         connexion.commit()
+         return {"message": f"Guitar succes... sucessfully destroy .... why?"}
     
     except: 
         raise HTTPException(
             status.HTTP_404_NOT_FOUND,
-            detail= "GUitar not found, guitar lost into the void T-T"
+            detail= "Guitar not found, guitar lost into the void T-T"
         )
 
 # PUT: ajouter mais sur l'existant
 
 @app.put("/guitars/{guitarId}", tags=["Guitars"])
 async def updateGuitar(guitarId: int, payload: Guitar, reposnse: Response):
+        
     try: 
-        guitarList[guitarId -1] = payload.dict()
+        cursor.execute('UPDATE guitar SET name=%s,brand=%s,type=%s,color=%s,price=%s,availability=%s  WHERE ID =%s;',(payload.guitarName, payload.guitarBrand, payload.guitarType, payload.guitarColor, payload.guitarPrice, payload.availability,guitarId))
+        connexion.commit()
         return  {"message" : f"Guitar successfully refine, bring more light into {payload.guitarName} "}
     except:
         raise HTTPException(
             status.HTTP_404_NOT_FOUND,
-            detail= "GUitar not found, guitar lost into the void T-T"
+            detail= "Guitar not found, guitar lost into the void T-T"
         )
